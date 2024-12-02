1a608c66535f559bdebf3e88ee9d8c7d621b6d5a
00001080122060sp <00001080122060sp@al.educacao.sp.gov.br>
2024-12-02 10:13:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é o País mais populozo do mundo?',
+    'India'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Estados Unidos?',
+    'A capital do EUA é Washington D.C.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o maior País em territorio do mundo?',
+    'Russia'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz TE amo em Inglês?',
+    ' Te amo em ingles é i love you'
+)
